0aa7f468943157fd2873254b8b3a977d78d24a47
Joe Karasek <joseph.karasek@gmail.com>
2016-11-03 11:00:21 -0700

    successfully set up an app

--- deploy.py
+++ deploy.py
@@ -12,12 +12,12 @@ else:
 	sys.exit("Your siteConfig.py file was not found. See example.siteconfig.py for more information on setting up siteConfig.py.")
 
 
-#Connect to webFaction API and Authenticate	
+#Connect to webFaction API and Authenticate
 server = xmlrpclib.ServerProxy('https://api.webfaction.com/')
 session_id, account = server.login(
-	siteConfig.userName, 
-	siteConfig.userPass, 
-	siteConfig.machineName, 
+	siteConfig.userName,
+	siteConfig.userPass,
+	siteConfig.machineName,
 	2)
 
 #Check for the existence of the site on the server
@@ -31,6 +31,32 @@ if websiteFound:
 	sys.exit()
 else:
 	print "Starting server configuration..."
+
+
+#Setting up the initial deploy
+#=============================
+
+#1. Create Webapp
+#Check for the existence of the site on the server
+appList = server.list_apps(session_id)
+appFound = False
+appName = siteConfig.websiteName + "_app"
+for app in appList:
+	if appName == app['name']:
+		appFound = True
+if appFound:
+	print "Warning: The webapp " + appName + " has already been created on the server."
+else:
+	print "Starting webApp configuration..."
+	server.create_app(
+		session_id,
+		appName,
+		'static_php56',
+		False,
+		'',
+		False)
+
+
 
 #cmd = "rm -f index.html && git clone https://github.com/HelloWorldDevs/lacey.git ."
 #test existence of webapp
@@ -54,8 +80,8 @@ else:
 #	server.create_app(session_id, siteConfig.webappName + "_app", 'static_php56', False, '', False)
 ##	Removes the dummy index.html from app creation
 #	server.system(session_id, "cd /home/danlinn/webapps/" + siteConfig.webappName + "_app" + " && rm -f index.html")
-#	
-#	
+#
+#
 #siteList = server.list_websites(session_id)
 #siteExists = False
 #for site in siteList:
@@ -71,11 +97,11 @@ else:
 #	appAddress = "/home/danlinn/webapps/" + siteConfig.webappName + "_app/"
 #	print appAddress
 #	server.create_website(
-#		session_id, 
-#		siteConfig.siteName, 
-#		"198.58.114.22", 
-#		False, 
-#		[siteConfig.siteName], 
+#		session_id,
+#		siteConfig.siteName,
+#		"198.58.114.22",
+#		False,
+#		[siteConfig.siteName],
 #		"",
 #		[[ siteConfig.webappName  + "_app", "http://" + siteConfig.siteName + ".hwdevs.site" ]]
 #		)
